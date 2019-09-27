ba7df93e3f6b2901e0405f9bd6ee7e1fc2bdb281
samaify <samaify.web@gmail.com>
2019-09-27 14:06:32 +0200

    update loading

--- src/Loading screen/loader.js
+++ src/Loading screen/loader.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     let title = $("#title");
     let loaderContainer = $("#loader-container");
+    let outerLoader = $("#outer-loader");
     let innerLoader = $("#inner-loader");
     let loadingText = $("#loading-text");
     let content = $("#content");
@@ -15,10 +16,10 @@ window.onload = function() {
     title.css("opacity", "1");
     title.css("margin-bottom", "0");
 
+
     title.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", () => {
+        load();
         loaderContainer.css("opacity", "1");
-        innerLoader.css("width", "100%");
-        setInterval(function(){ console.log(innerLoader.width("%") / innerLoader.parent().width() * 100) }, 1);
     });
 
     innerLoader.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", () => {
@@ -48,7 +49,7 @@ window.onload = function() {
         }
     });
     features.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", () => {
-        features.css("transition", "all .3s ease-in"); /*da nueva propiedad de transicion a las boxes para que las transiciones en hover sean de 0.3 en lugar del 0.8s inicial*/
+        features.css("transition", "all .3s ease-in-out"); /*da nueva propiedad de transicion a las boxes para que las transiciones en hover sean de 0.3 en lugar del 0.8s inicial*/
     });
 
 
@@ -59,28 +60,16 @@ window.onload = function() {
             $(this).siblings().css("width", "0");
             showFeatureBoxes($(this));
             removeFeaturesHoverClass($(this), 800);
-            hiddenFeatures = false;
         }
         else{
             features.not($(this)).css("opacity", "0");
-            $(this).addClass("features-hover-class");
+            featuresText.css("transform", "translateY(-25vh)");
+            $(this).addClass("features-click-class");
             hideFeatureBoxes($(this));
             createDesplegableInfo($(this));
             hiddenFeatures = true;
         }
     });
-
-    features.on("mouseenter", function() {
-        $(this).addClass("features-hover-class");
-    });
-    features.on("mouseleave", function() {
-        if(!hiddenFeatures){ //if visible
-            features.css("opacity", "0.7");
-            removeFeaturesHoverClass($(this), 0);
-        }
-    });
-
-
 
 
 
@@ -98,6 +87,7 @@ window.onload = function() {
         setTimeout(function(){
             currentElement.closest("#features-container").css("justify-content", "space-between");
             features.not(currentElement).parent().css("display", "block")
+            features.css("transition", "all .8s ease-in");
             features.css("opacity", "0.7"); //WHY TRANSITION NOT APPLIED TO THIS LINEEEEEEEEEE WTFFF
         }, 1000); //.8s is the transition time of the info dropdown
     }
@@ -108,11 +98,25 @@ window.onload = function() {
     }
     function removeFeaturesHoverClass(currentElement, delay){
         setTimeout(function(){
-            currentElement.removeClass("features-hover-class");
+            currentElement.removeClass("features-click-class");
+            featuresText.css("transform", "translateY(-21vh)");
+            hiddenFeatures = false;
         }, delay); //.8s is the transition time of the info dropdown
     }
+    function load() {
+        innerLoader.width("100%");
+        let width = 0;
+        let id = setInterval(frame, 10);
+        function frame() {
+            console.log(width)
+            outerLoader.find("p").html(width + "%");
+            if (width == 100) {
+                clearInterval(id);
+            } else {//meter la formula
+                console.log("entro")
+                width = innerLoader.width() / innerLoader.parent().width() * 100;;
+                innerLoader.width(width + '%');
+            }
+        }
+    }
 }
-
-
-
-/*setInterval(() => trainLine.css("display", "block"), 500);*/
