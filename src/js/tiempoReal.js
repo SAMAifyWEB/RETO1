2010e6ca30d0637770974cee4ca31cd40651e124
samaify <samaify.web@gmail.com>
2019-10-02 13:08:33 +0200

    tren

--- src/js/tiempoReal.js
+++ src/js/tiempoReal.js
@@ -1,5 +1,8 @@
 window.onload = function() {
     let trainLine = $("#train-line");
     let train = $(".train");
+    let train1 = $("#train1");
+    $(".subway-map").subwayMap();
 
+    train1.css({"left" : "100%", "transform" : "translateX(-50%)"});
 }
