66b7a952d2a042ac8872d19503001444addc33f9
SAMAify <samaify.web@gmail.com>
2019-10-02 13:05:19 +0200

    grafica

--- src/js/grafica.js
+++ src/js/grafica.js
@@ -1,18 +1,34 @@
 //documento usado unicmanete para la creacion de graficas
 
+let contador
+let velocidad
+let aceleracion
+
+$(document).ready(function () {
+    setInterval(actualizar, 5000);
+})
+
+function actualizar() {
+    $("#valores").load("leer_variable.html");
+}
+
+contador = parseInt($("#valores").load("leer_variable.html"))
+
+alert(contador)
+
 let miCanvas = document.getElementById("miGrafica").getContext("2d")
 
 let chart = new Chart(miCanvas, {
     type: "line",
     data: {
-        labels: ["Velocidad", "Distancia", "Prueba"],
+        labels: ["Contador", "Velocidad", "Aceleracion"],
         datasets: [
             {
                 label: "Grafica del tranvia",
                 backgroundColor: "rgb(0,0,0)",
                 borderColor: "rgb(0,250,0)",
                 borderWidth: 2.5,
-                data: [27, 50, 10]
+                data: [contador]
             }
         ]
     }
