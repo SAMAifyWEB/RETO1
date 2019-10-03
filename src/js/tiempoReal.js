c60eadb3fb4f06072ea28835174036e7f7ab4cc9
samaify <samaify.web@gmail.com>
2019-10-03 14:07:15 +0200

    .

--- src/js/tiempoReal.js
+++ src/js/tiempoReal.js
@@ -3,6 +3,13 @@ window.onload = function() {
     let train = $(".train");
     let train1 = $("#train1");
     $(".subway-map").subwayMap();
+    let span = $(".text");
 
-    train1.css({"left" : "100%", "transform" : "translateX(-50%)"});
+    span.eq(0).css({"left" : "40", "top" : "80"})
+    span.eq(1).css({"left" : "240", "top" : "160"})
+    span.eq(2).css({"left" : "361", "top" : "41"})
+    span.eq(3).css({"left" : "361", "top" : "243"})
+
+    //train1.css({"left" : "100%", "transform" : "translateX(-50%)"});
+
 }
