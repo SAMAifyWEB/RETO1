2c9bb31a87f022884c40f85a20a1a5552a0d9f98
Aritz Martínez <aritz.martinezh@gmail.com>
2019-10-07 13:13:55 +0200

    comentario js

--- src/js/funciones.js
+++ src/js/funciones.js
@@ -1,3 +1,7 @@
+/*Las variables Bool en tia portal muestran un true/false
+  Internamente son 1 y 0 por lo que al mostrarlas en la pagina html se muestra un 0 o un 1
+  para poder hacer la combersion leo el dato lo guardo en una variable y lo combierto a true/false respectivamente.
+*/
 $(document).ready(function() {
     setInterval(actualizar,500);
     let s = parseInt(document.getElementById("bool").innerHTML);
