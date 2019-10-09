f3c8f2638e1f45fbc6f601c0c4acf5e7af217cfe
Aritz Martínez <aritz.martinezh@gmail.com>
2019-10-09 15:17:37 +0200

    documentos

--- src/js/template.js
+++
@@ -1,8 +0,0 @@
-
-window.onload = function () {
-    var template = document.getElementById("plantillaMenu");
-    console.log(template)
-    let clone = template.content.cloneNode(true);
-    console.log(document.body);
-    document.body.appendChild(clone);
-}
